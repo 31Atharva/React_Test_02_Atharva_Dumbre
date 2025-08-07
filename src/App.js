3c7e6b83ee460275b847dc0b3f5c21bf9fbf5325
Atharva Dumbre <84585013+31Atharva@users.noreply.github.com>
2025-08-07 18:03:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react"
+import LeadsTable from "./components/LeadsTable";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LeadsTable />
+    </div>
+  );
+}
+
+export default App;
